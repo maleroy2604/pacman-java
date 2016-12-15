d60e764cd3ff281f97fb68587b962e44c8f262ca
Martin <>
2016-12-15 18:28:03 +0100

    pdf class domain et debut class loigiciel avec debut implémentation code.

---
+++ src/Model/Case.java
@@ -0,0 +1,11 @@
+
+package Model;
+
+
+public class Case {
+    Element elem;
+    
+    public  Case(Element elem){
+        this.elem=elem;
+    }
+}
---
+++ src/Model/Element.java
@@ -0,0 +1,11 @@
+
+package Model;
+
+
+public abstract class Element {
+    private int x;
+    private int y;
+    
+    
+    
+}
